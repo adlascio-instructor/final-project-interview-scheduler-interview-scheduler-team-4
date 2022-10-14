786a77181404568372537c045f9997960eba39aa
Jorge Ruiz <97487049+jorgeruizm94@users.noreply.github.com>
2022-10-14 00:20:25 -0700

    server set up v1

--- server.js
+++ server.js
@@ -1,12 +1,63 @@
 const express = require("express");
+const { Pool } = require("pg");
+
+const server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true}));
+
+
+const dbCredentials = {
+    user: "jorgeruiz",
+    host: "localhost",
+    database: "interviewer_scheduler",
+    password: "",
+    port: 5432,
+  };
+
+server.get("/interviews", (req, res) => {
+    const pool = new Pool(dbCredentials);
+    pool
+      .query("SELECT * FROM interviews")
+      .then((result) => result.rows)
+      .then((interviews) => res.json(interviews))
+      .catch((err) => console.log(err))
+      .finally(() => pool.end());
+  });
+
+server.get("/interviewers", (req, res) => {
+    const pool = new Pool(dbCredentials);
+    pool
+      .query("SELECT * FROM interviewers")
+      .then((result) => result.rows)
+      .then((interviewers) => res.json(interviewers))
+      .catch((err) => console.log(err))
+      .finally(() => pool.end());
+  });
+
+
+  server.get("/available_interviewers", (req, res) => {
+    const pool = new Pool(dbCredentials);
+    pool
+      .query("SELECT * FROM available_interviewers JOIN days WHERE ") /* Research how to get AV INT for a given day */
+      .then((result) => result.rows)
+      .then((available_interviewers) => res.json(available_interviewers))
+      .catch((err) => console.log(err))
+      .finally(() => pool.end());
+  }); 
+/*
 const availableDayRoutes = require ("./routes/availableDay");
 const interviewerRoutes = require ("./routes/interviewer");
 
-const app = express();
 
-app.use(availableDayRoutes)
-app.use(interviewerRoutes)
+server.use(availableDayRoutes)
+server.use(interviewerRoutes)
+
 
 const port = 8000;
+*/
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+
+server.listen(8000, () => console.log(`Server is running on port 8000`));
